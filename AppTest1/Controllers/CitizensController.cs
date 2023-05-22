e495a7c50f7f8ea5a20c8bd475c7e5c34bf97c1e
Ahmad Damra <ahmeddamra_cs@hotmail.com>
2023-05-22 10:25:35 +0300

    Add project files.

---
+++ AppTest1/Controllers/CitizensController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AppTest1.Models;
+
+namespace AppTest1.Controllers
+{
+    public class CitizensController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CitizensController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Citizens
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Citizens.ToListAsync());
+        }
+
+        // GET: Citizens/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var citizen = await _context.Citizens
+                .FirstOrDefaultAsync(m => m.CitizenId == id);
+            if (citizen == null)
+            {
+                return NotFound();
+            }
+
+            return View(citizen);
+        }
+
+        // GET: Citizens/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Citizens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CitizenId,DBRNumber,CitizenName,Gender,BirthType,NumberOfBorn,BOD,BODText,FatherName,FatherAge,FatherWork,MotherName,City,Side,Eliminate,Governorate,DepartmentOfHealth,QRCode")] Citizen citizen)
+        {
+            if (ModelState.IsValid)
+            {
+                citizen.CitizenId = Guid.NewGuid();
+                _context.Add(citizen);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(citizen);
+        }
+
+        // GET: Citizens/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var citizen = await _context.Citizens.FindAsync(id);
+            if (citizen == null)
+            {
+                return NotFound();
+            }
+            return View(citizen);
+        }
+
+        // POST: Citizens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("CitizenId,DBRNumber,CitizenName,Gender,BirthType,NumberOfBorn,BOD,BODText,FatherName,FatherAge,FatherWork,MotherName,City,Side,Eliminate,Governorate,DepartmentOfHealth,QRCode")] Citizen citizen)
+        {
+            if (id != citizen.CitizenId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(citizen);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitizenExists(citizen.CitizenId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(citizen);
+        }
+
+        // GET: Citizens/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var citizen = await _context.Citizens
+                .FirstOrDefaultAsync(m => m.CitizenId == id);
+            if (citizen == null)
+            {
+                return NotFound();
+            }
+
+            return View(citizen);
+        }
+
+        // POST: Citizens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var citizen = await _context.Citizens.FindAsync(id);
+            _context.Citizens.Remove(citizen);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitizenExists(Guid id)
+        {
+            return _context.Citizens.Any(e => e.CitizenId == id);
+        }
+    }
+}
